4226c6224cad3eb0de5ce64ab213a0c922951cf9
vbv-nyk <vbv.nyk.2003@gmail.com>
2023-11-28 15:37:59 +0530

    Change endpoints to listen to battery percentage

--- server.js
+++ server.js
@@ -3,13 +3,54 @@ const batteryLevel = require('battery-level');
 
 const app = express();
 
-let battery;
-app.get("/battery-stats", async (req,res) => {
-    let battery = await batteryLevel() * 100;
-    console.log(battery);
-    res.send(JSON.stringify({
-        "battery": battery,
-    }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+let battery = 20, should_charge = true, ll = 40, ul = 80;
+
+
+function check_charge(){
+    if(battery >= ll && battery <= ul) {
+        return true
+    }  else {
+        return false
+    }
+}
+
+app.post("/update-battery", (req,res) => {
+    let {laptop_battery} = req.body;
+    battery = Math.floor(laptop_battery);
+    should_charge = check_charge()
+    console.log(`update-battery ${ll} ${ul} ${should_charge} ${battery}`)
+    res.send({
+        response:"Success",
+        should_charge,
+        ll,
+        ul,
+        battery
+    })
+})
+
+app.post("/change-bounds", (req,res) => {
+    let {lower_limit, upper_limit} = req.body;
+    ll = lower_limit
+    ul = upper_limit
+    should_charge = check_charge();
+    console.log(`update-battery ${ll} ${ul} ${should_charge} ${battery}`)
+    res.send({
+        ll,
+        ul,
+        should_charge  
+    })
+})
+
+app.get("/battery-stats", (req,res) => {
+    console.log(`battery-stats ${ll} ${ul} ${should_charge} ${battery}`)
+    res.send({
+        ll,
+        ul,
+        should_charge  
+    })
 })
 
 app.listen(8080,()=> {
